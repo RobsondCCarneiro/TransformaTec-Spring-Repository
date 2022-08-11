14338789652f464fe846de22441b64fe2e76ce40
RobsondCCarneiro <robinho.cenep@gmail.com>
2022-08-11 12:05:33 -0300

    Implantando o padrão Repository

---
+++ src/test/java/com/aula110/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aula110.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
